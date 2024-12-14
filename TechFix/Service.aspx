a6ab2abc83e67c37893087de8c84a59273e8f2a0
randiPalguna <158301222+randiPalguna@users.noreply.github.com>
2024-12-14 20:17:33 +0700

    chore: initial commit

---
+++ TechFix/Service.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Service.aspx.cs" Inherits="_Service" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Show the grid</title>
+</head>
+<body>
+    <form id="form1" OnInit="Page_Load" runat="server">
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ TechFix/Service.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+public partial class _Service : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Check if user is logged in
+        //if (Session["Username"] == null)
+        //{
+        //    Response.Redirect("Login.aspx"); // Redirect to login if session is empty
+        //}
+
+        // Database connection string
+        string cs = "server=localhost;userid=root;password=;database=TechFix";
+        string query = "SELECT * FROM Service";
+
+        using (MySqlConnection con = new MySqlConnection(cs))
+        {
+            try
+            {
+                con.Open(); // Open the connection
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Service");
+                if (GridView1 != null)
+                {
+                    GridView1.DataSource = ds.Tables["Service"].DefaultView;
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or handle exception
+                Response.Write("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close(); // Close the connection
+            }
+        }
+    }
+}
